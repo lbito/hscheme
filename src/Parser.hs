9e7fda847ddf557e822555ed23d52df6edb561ec
lbito <lbit@protonmail.com>
2018-05-05 15:31:37 +1000

    added Char, Oct, Hex, Binary, Float and list support to Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,24 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Numeric
+import Data.Char (digitToInt)
+import Data.List (foldl')
 
 data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
+             | Float Double
              | Number Integer
              | String String
              | Bool Bool
+             | Character Char
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
+bin2dec :: String -> Integer
+bin2dec = foldr (\c s -> s * 2 + c) 0 . reverse . map c2i
+    where c2i c = if c == '0' then 0 else 1
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -18,12 +26,22 @@ spaces = skipMany1 space
 escapedChars :: Parser Char
 escapedChars = do char '\\'
                   x <- oneOf "\\\"nrt"
-                  return case x of
+                  return $ case x of
                     '\\' -> x
                     '\"' -> x
                     'n'  -> '\n'
                     'r'  -> '\r'
                     't'  -> '\t'
+
+parseCharacter :: Parser LispVal
+parseCharacter = do
+  try $ string "#\\"
+  value <- try (string "newline" <|> string "space") 
+           <|> do { x <- anyChar; notFollowedBy alphaNum ; return [x] }
+  return $ Character $ case value of
+    "space" -> ' '
+    "newline" -> '\n'
+    otherwise -> (value !! 0)
 
 parseString :: Parser LispVal
 parseString = do
@@ -37,19 +55,71 @@ parseAtom = do
               first <- letter <|> symbol
               rest <- many (letter <|> digit <|> symbol)
               let atom = first:rest
-              return $ case atom of
-                "#t" -> Bool True
-                "#f" -> Bool False
-                _    -> Atom atom
+              return $ Atom atom
+
+parseBool :: Parser LispVal
+parseBool = do
+               first <- try $ char '#'
+               val <- try $ oneOf ("tf")
+               return $ case val of
+                 't' -> Bool True
+                 'f' -> Bool False
+
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+              x <- many1 hexDigit
+              return $ Number . fst $ readHex x !! 0
+
+parseOct :: Parser LispVal
+parseOct = do try $ string "#o"
+              x <- many1 octDigit
+              return $ Number . fst $ readOct x !! 0
+
+parseBin :: Parser LispVal
+parseBin = do try $ string "#b"
+              x <- many1 (oneOf "10")
+              return $ Number (bin2dec x)
+
+parseLiteralDecimal :: Parser LispVal
+parseLiteralDecimal = many1 digit >>= return . Number . read
+
+parseDecimal :: Parser LispVal
+parseDecimal = do try $ string "#d"
+                  x <- many1 digit
+                  (return . Number . read) x
+
+parseFloat :: Parser LispVal
+parseFloat = do x <- many1 digit
+                char '.'
+                y <- many1 digit
+                return $ Float (fst.head$readFloat (x++"."++y))
 
 parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= return . Number . read
+parseNumber = parseLiteralDecimal <|> parseHex <|> parseDecimal <|> parseOct <|> parseBin
 
+
+parseList :: Parser LispVal
+parseList = (sepBy parseExpr spaces) >>= \x -> (return . List) x
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
-         <|> parseNumber
+         <|> try parseFloat
+         <|> try parseNumber
+         <|> try parseBool
+         <|> try parseCharacter
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
